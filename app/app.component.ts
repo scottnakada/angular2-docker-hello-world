d197ef50705a173644a8aac88b14e739b3c82112
Scott Nakada <scottnakada@gmail.com>
2016-09-25 14:14:04 -0700

    Add the Angular2 docker hello-world code

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello World!</h1><h2>My First Angular App</h2>'
+})
+export class AppComponent { }
+
